e2cb948095d5eb47550ae3f4333ffafa2c20a11b
rom3lll <59618516+rom3lll@users.noreply.github.com>
2020-09-10 14:03:06 -0300

    Update RunningSumof1dArray1480.java

--- LeetCode/RunningSumof1dArray1480.java
+++ LeetCode/RunningSumof1dArray1480.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int[] runningSum(int[] nums) {
+    int auxiliar=0;
+		int [] vector_respuesta=new int[nums.length];
+		vector_respuesta[0]=nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			auxiliar=auxiliar+nums[i-1];
+			vector_respuesta[i]=nums[i]+auxiliar;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			nums[i]=vector_respuesta[i];
+		}
+    	return nums;
+    }
+}
